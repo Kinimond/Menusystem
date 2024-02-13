1c997e053f855cc678706cf06fe6a50f2e3b5edd
Patrick <patrickhass84@gmail.com>
2024-02-13 11:13:54 +0100

    Add project files.

---
+++ Menusystem/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menusystem
+{
+    internal class Menu
+    {
+
+        public string Title;
+        private MenuItem[] MenuItems = new MenuItem[10]; 
+        private int ItemCount = 0;
+
+        public Menu(string title)
+        {
+            this.Title = title;
+        }
+        public void Show() {
+            Console.WriteLine("Min fantastiske menu \n");
+            for (int i = 0; i < MenuItems.Length; i++)
+            {
+                if (MenuItems[i] != null)
+                {
+                    Console.WriteLine(MenuItems[i].Title);
+                }
+            } 
+        }
+        public void AddMenuItem(string menuTitle)
+        {
+            MenuItem mi = new MenuItem(menuTitle);
+            MenuItems[ItemCount] = mi;
+            ItemCount++;
+        }
+        public int SelectMenuItem(int selectedMenuItem) {
+            while(selectedMenuItem = )
+            {
+                int id = int.Parse(Console.ReadLine());
+            }
+
+            return id;
+        }
+    }
+}
---
+++ Menusystem/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Menusystem
+{
+    internal class Program
+    {       static void Main(string[] args)
+            {
+                Menu mainMenu = new Menu("Min fantastiske menu");
+                    
+
+                // First menu item
+                MenuItem mi = new MenuItem("1. Gør dit");
+                mainMenu.AddMenuItem(mi.Title); 
+          
+                // Second menu item
+                mi = new MenuItem("2. Gør dat");
+                mainMenu.AddMenuItem(mi.Title);
+
+                // Third menu item
+                mi = new MenuItem("3. Gør noget");
+                mainMenu.AddMenuItem(mi.Title);
+
+                // Last menu item
+                mi = new MenuItem("4. Få svaret på livet, universet og alting");
+                mainMenu.AddMenuItem(mi.Title);
+
+                mainMenu.Show();
+
+                Console.ReadLine();
+        }
+    }
+}
